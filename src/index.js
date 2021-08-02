9025a4dcda33a45c43670659f578bece533b9006
Lucas Hubert <lucashu@live.com.ar>
2021-08-02 03:01:15 -0300

    Conditions data

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const port = 3000
 
 app.use(express.json())
 
-var newUserID = 1000;
-var usersDB = {};
+var newUserID = 1000
+var usersDB = {}
 
 app.post('/users', (req, res) =>{
+    // Condition data nickname
+    if(!req.body || !req.body.nickname || typeof req.body.nickname != "string" || req.body.nickname.length === 0 ){
+        res.sendStatus(400)
+        return
+    }
+
     // Create new user
     var newUser = {
         id: newUserID,
@@ -19,19 +25,38 @@ app.post('/users', (req, res) =>{
 
     // Change unique ID
     newUserID++
-    console.log(req.body);
-    res.send(newUser)
+    res.status(201).json(newUser)
 })
 
 app.get('/users/:id', (req, res) => {
+    // Condition data ID
+    if(!req.params.id || req.params.id.length === 0){
+        res.sendStatus(400)
+        return
+    }
+
     var user = usersDB[req.params.id]
+
+    if(!user){
+        res.sendStatus(404)
+        return
+    }
 
     res.send(user)
 })
 
 app.put('/users/:id', (req, res) =>{
-    var updatedUser = usersDB[req.params.id];
-    updatedUser.nickname = req.body.nickname;
+    if(!req.params.id){
+        res.sendStatus(400)
+        return
+    }
+
+    var updatedUser = usersDB[req.params.id]
+    if(!updatedUser){
+        res.sendStatus(404)
+        return
+    }
+    updatedUser.nickname = req.body.nickname
     usersDB[req.params.id] = updatedUser
     res.send(updatedUser)
 })
